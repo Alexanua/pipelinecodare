590913ad500bb8438f03dadc854c5856762b2d8d
Alexanua <86alesha@gafe.molndal.se>
2024-02-18 15:53:29 +0100

    Remove pipelinecodare-main-1 directory

---
+++ Downloads/pipelinecodare-main-1/src/main/java/io/MajdiCodarePipeline/pipelinecodare/PipelinecodareApplication.java
@@ -0,0 +1,13 @@
+package io.MajdiCodarePipeline.pipelinecodare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipelinecodareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PipelinecodareApplication.class, args);
+	}
+
+}
