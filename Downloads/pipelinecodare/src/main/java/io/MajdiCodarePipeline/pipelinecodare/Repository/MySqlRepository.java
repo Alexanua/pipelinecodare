2c4a50f4fefa81e4a5b26db8a2df692af0be5041
Alexanua <86alesha@gafe.molndal.se>
2024-02-18 15:35:50 +0100

    delet

--- Downloads/pipelinecodare/src/main/java/io/MajdiCodarePipeline/pipelinecodare/Repository/MySqlRepository.java
+++
@@ -1,9 +0,0 @@
-package io.MajdiCodarePipeline.pipelinecodare.Repository;
-
-import io.MajdiCodarePipeline.pipelinecodare.entity.Message;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MySqlRepository extends JpaRepository<Message, Long> {
-}
