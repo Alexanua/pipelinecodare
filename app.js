7fb7a76ebce285b42bcabbd6e4fd58202dbf241e
Alexanua <86alesha@gafe.molndal.se>
2024-02-21 18:25:02 +0100

    Skapa .js och uppdatera konfigurationer

---
+++ app.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+const express = require('express');
+const mysql = require('mysql2');
+
+const app = express();
+
+// Skapar databasanslutningen med konfiguration från .env-filen
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+});
+
+// Försöker ansluta till databasen
+db.connect(err => {
+    if (err) {
+        console.error('Databasanslutningsfel: ' + err.message);
+        return;
+    }
+    console.log('Framgångsrikt ansluten till databasen');
+});
+
+// Definierar en grundläggande rutt
+app.get('/', (req, res) => {
+    res.send('Hej från Node.js på AWS!');
+});
+
+// Startar servern på port specificerad i .env, eller 8086 som fallback
+const PORT = process.env.PORT || 8086;
+app.listen(PORT, () => {
+    console.log(`Servern kör på port ${PORT}`);
+});
---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mysql = require('mysql2');
+
+const app = express();
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'Majdi_db',
+    password: "" ,
+});
+
+db.connect(err => {
+    if (err) {
+        console.error('Databasanslutningsfel: ' + err.message);
+        return;
+    }
+    console.log('Framgångsrikt ansluten till databasen');
+});
+
+app.get('/', (req, res) => {
+    res.send('Hej från Node.js på AWS!');
+});
+
+const PORT = process.env.PORT || 8086;
+app.listen(PORT, () => {
+    console.log(`Servern kör på port ${PORT}`);
+});
